5ff805c015309b4877a377550bdb1c6257387a5d
jhsee <jhsee11@gmail.com>
2020-10-31 22:43:44 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t seejiahao/multi-client:latest -t seejiahao/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t seejiahao/multi-server:latest -t seejiahao/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t seejiahao/multi-worker:latest -t seejiahao/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push seejiahao/multi-client:latest
 docker push seejiahao/multi-server:latest
 docker push seejiahao/multi-worker:latest
