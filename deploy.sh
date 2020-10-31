8c649264431a7a31705358d26a7e90652a050e03
jhsee <jhsee11@gmail.com>
2020-10-31 22:16:27 +0800

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t seejiahao/multi-client:latest -t seejiahao/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t seejiahao/multi-server:latest -t seejiahao/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t seejiahao/multi-worker:latest -t seejiahao/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push seejiahao/multi-client:latest
+docker push seejiahao/multi-server:latest
+docker push seejiahao/multi-worker:latest
+
+docker push seejiahao/multi-client:$SHA
+docker push seejiahao/multi-server:$SHA
+docker push seejiahao/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=seejiahao/multi-server:$SHA
+kubectl set image deployments/client-deployment client=seejiahao/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=seejiahao/multi-worker:$SHA
